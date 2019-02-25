71d4160b6e07707e3bafccca932ee4797ea0d317
Omar <Omar.abdi.2022@student.lhschools.org>
2019-02-25 08:05:12 -0500

    Done and ready
    The first one helped me get into feel of it and now i just want to figure out how to put a scanner into the method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,23 +3,29 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        println();
+        }
+    public static void println() { System.out.println("Hello World");
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
