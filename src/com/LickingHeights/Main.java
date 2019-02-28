b1f2e27252c6018b3405aa0b04edef99a34ca3c9
Omar <Omar.abdi.2022@student.lhschools.org>
2019-02-28 07:35:32 -0500

    More than needed
    At first i did more than what was necessary and had to go0 back and delete of unneeded code i want to be better at the next method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,13 +1,31 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        println();
-        }
-    public static void println() { System.out.println("Hello World");
-
+public static void main(String[] args) {
+    //declare
+    //initializing
+   //System.out.println("What is your name?");
+    println();
+    System.out.println("What is your name?");
+    prints();
     }
+public static void println() { System.out.println("Hello World");
+}
+public static void prints() {
+    //declare
+    String Name;
+    Scanner input;
+    //initializing
+    input = new Scanner(System.in);
+    Name = input.nextLine();
+    System.out.println("hello "+ Name);
+
+
+
+}
 
 
 
