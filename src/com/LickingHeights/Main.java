0da040aafa9ac1254fb658a659ebd74cbfdfb1bf
Omar <Omar.abdi.2022@student.lhschools.org>
2019-03-01 08:01:18 -0500

    pretty ez but..
    pretty easy but it took a minute for me to understand it better

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
-public static void main(String[] args) {
-    //declare
-    //initializing
-   //System.out.println("What is your name?");
-    println();
-    System.out.println("What is your name?");
-    prints();
+    public static void main(String[] args) {
+        //declare
+        Scanner input;
+        //initializing
+        input = new Scanner(System.in);
+        println();
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        prints(name);
+        multiply(7, 6); }
+
+        public static void println() {
+        System.out.println("Hello World");
     }
-public static void println() { System.out.println("Hello World");
-}
-public static void prints() {
-    //declare
-    String Name;
-    Scanner input;
-    //initializing
-    input = new Scanner(System.in);
-    Name = input.nextLine();
-    System.out.println("hello "+ Name);
+    public static void prints(String name) {
+        //declare
+        System.out.println("hello " + name);
+    }
+    public static int multiply(int A, int B) {
+        System.out.println("7x6= " + A * B);
+        return A;
 
 
 
@@ -45,5 +48,8 @@ public static void prints() {
 
 
 
-}
+
+
+
+
 
